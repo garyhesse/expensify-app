fa6d4d84f0ad045e415582d2f761ab9b5e75e025
Gary Hesse <hesse@wenatcheeworld.com>
2018-05-09 13:54:23 -0700

    Add Expensify styles

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,8 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from '../actions/auth';
 
 export const LoginPage = ({ startLogin }) => (
-    <div>
-        <button onClick={startLogin}>Login</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expensify</h1>
+            <p>It's time to get your expenses under control</p>
+            <button className="button" onClick={startLogin}>Login with Google</button>
+        </div>
     </div>
 );
 
@@ -13,34 +17,3 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
-
-// export default class LoginPage extends React.Component {
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-//             username: props.expense ? props.expense.username : '',
-//             password: props.expense ? props.expense.password : '',
-//         };
-//     };
-//     render() {
-//         return (
-//             <div>
-//                 <form>
-//                     <input
-//                         type="text"
-//                         placeholder="Username"
-//                         autoFocus
-//                         value={this.state.username}
-//                     />
-//                     <input
-//                         type="password"
-//                         placeholder="Password"
-//                         value={this.state.password}
-//                     />
-//                     <button>Login</button>
-//                 </form>
-//             </div>
-//         )
-//     };
-// };
